83cf5c6e3e4cac39a0f6f6ca8e9da5592f79c6fe
azalhu <azalhu@icloud.com>
2024-10-18 09:57:49 +0200

    create item in the database

--- manufacturing/db/tests/items_test.rs
+++ manufacturing/db/tests/items_test.rs
@@ -1,4 +1,4 @@
-use db::grpc::items::{item_client::ItemClient, ItemRequest};
+use db::inbound::grpc::item::{item_client::ItemClient, ItemRequest};
 use tonic::Request;
 
 #[tokio::test]
@@ -6,13 +6,15 @@ async fn it_creates_new_item() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = ItemClient::connect("http://localhost:1339").await?;
 
     let request = Request::new(ItemRequest {
-        code: "Hello".to_owned(),
+        number: "P10000".to_owned(),
+        name: "Pedal".to_owned(),
     });
 
     let response = client.create_item(request).await?.into_inner();
 
-    assert_eq!(response.id, 0);
-    assert_eq!(response.code, "Hello");
+    assert_eq!(response.id, "0");
+    assert_eq!(response.number, "P10000");
+    assert_eq!(response.name, "Pedal");
 
     Ok(())
 }
