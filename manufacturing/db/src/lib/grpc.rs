07cc6bfb64249b070afa066e1fdd40ed92ed9502
azalhu <azalhu@icloud.com>
2024-11-05 19:01:43 +0100

    build

--- manufacturing/db/src/lib/grpc.rs
+++ manufacturing/db/src/lib/grpc.rs
@@ -1,11 +1,1 @@
-use std::sync::Arc;
-
-use tonic::transport::server::Router;
-
-use crate::Connection;
-
 pub mod item;
-
-pub trait ItemServices<DB: Connection> {
-    fn add_item_services(self, db_connection: Arc<DB>) -> Router;
-}
